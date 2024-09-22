ff4554bd602d312a1e628114a680dbe92fcb4ff7
AetherPhx <aetherphx@gmail.com>
2024-09-21 19:38:10 -0500

    Crear componente NavMenu

---
+++ src/components/NavMenu/NavMenu.jsx
@@ -0,0 +1,11 @@
+export function NavMenu() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
