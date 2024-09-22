09c6851df5331cd1d9b6461252fa0b561608cb2a
retosemana3 <susansharmely2@gmail.com>
2024-09-21 23:27:07 -0500

    finalizar info final

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,18 +1,17 @@
 import "./Info.css";
 
-<<<<<<< HEAD
 export function Info(){
     return(
-        <section class="info">
-          <div class="info-content">
-              <div class="info-content-text">
+        <section class="Info">
+          <div class="Info-content">
+              <div class="Info-content-text">
                   <h2>Discover</h2>
                   <h3>our story</h3>
-                  <p  class="info-content-parrafo"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptatum magnam temporibus dolorum, fuga commodi.</p>
+                  <p  class="Info-content-parrafo"> Lorem ipsum dolor sit amet consectetur adipisicIng elit. Numquam voluptatum magnam temporibus dolorum, fuga commodi.</p>
                   <a href="#">Lear more</a>
               </div>
-              <div class="info-content-img">
-                  <div class="info-content-img_uno">
+              <div class="Info-content-img">
+                  <div class="Info-content-img_uno">
                    <img src="/src/asest/img/story-img.jpg" alt="imagen"/>
 
                   </div>
@@ -25,45 +24,3 @@ export function Info(){
     </section>
     )
 }
-=======
-export function Info() {
-	return (
-		<section className="info">
-			<div className="info__content">
-				<div className="info__content__text">
-					<h2>Discover</h2>
-					<h3>our story</h3>
-					<p className="info__content__parrafo">
-						{" "}
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-						voluptatum magnam temporibus dolorum, fuga commodi.
-					</p>
-					<a href="#">Lear more</a>
-				</div>
-				<div className="info__content__img">
-					<div className="info__content__img_uno">
-						<img src="/src/asest/img/story-img.jpg" alt="imagen" />
-					</div>
-				</div>
-			</div>
-			<div className="info__content">
-				<div className="info__content__img">
-					<div className="info__content__img_uno">
-						<img src="/src/asest/img/story-img.jpg" alt="imagen" />
-					</div>
-				</div>
-				<div className="info__content__text">
-					<h2>Explore</h2>
-					<h3>menu</h3>
-					<p className="info__content__parrafo">
-						{" "}
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-						voluptatum magnam temporibus dolorum, fuga commodi.
-					</p>
-					<a href="#">see full menu</a>
-				</div>
-			</div>
-		</section>
-	);
-}
->>>>>>> main
