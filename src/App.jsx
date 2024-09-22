937233d7918fc3606ce82e51e25bbc04b282878c
retosemana3 <susansharmely2@gmail.com>
2024-09-21 22:50:59 -0500

    finalizar info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Banner } from './components/Banner';
 import { Header } from "./Components/Header/Header";
-
+import { Info } from "./components/Info";
 export function App() {
   return (
     <div className="layout">
--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -21,25 +21,7 @@ export function Info(){
              
 
           </div>
-          <div class="info__content">
-            
-              <div class="info__content__img">
-                  <div class="info__content__img_uno">
-                   <img src="/src/asest/img/story-img.jpg" alt="imagen"/>
-
-                  </div>
-
-              </div>
-              <div class="info__content__text">
-                  <h2>Explore</h2>
-                  <h3>menu</h3>
-                  <p  class="info__content__parrafo"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptatum magnam temporibus dolorum, fuga commodi.</p>
-                  <a href="#">see full menu</a>
-              </div>
-             
-
-          </div>
-
+          
     </section>
     )
 }
