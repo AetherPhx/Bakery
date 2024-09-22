e66917bf0c2a5f05c08c97f6dbdad83d5f77bd04
retosemana3 <susansharmely2@gmail.com>
2024-09-21 23:03:16 -0500

    finalizar info

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -4,15 +4,15 @@ import "./Info.css";
 export function Info(){
     return(
         <section class="info">
-          <div class="info__content">
-              <div class="info__content__text">
+          <div class="info-content">
+              <div class="info-content-text">
                   <h2>Discover</h2>
                   <h3>our story</h3>
-                  <p  class="info__content__parrafo"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptatum magnam temporibus dolorum, fuga commodi.</p>
+                  <p  class="info-content-parrafo"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptatum magnam temporibus dolorum, fuga commodi.</p>
                   <a href="#">Lear more</a>
               </div>
-              <div class="info__content__img">
-                  <div class="info__content__img_uno">
+              <div class="info-content-img">
+                  <div class="info-content-img_uno">
                    <img src="/src/asest/img/story-img.jpg" alt="imagen"/>
 
                   </div>
