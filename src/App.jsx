03aa87284ea8dd0ceb5f5b330b05e68f9b9b0a7a
retosemana3 <susansharmely2@gmail.com>
2024-09-21 23:21:05 -0500

    finalizar info merge

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { Header } from "./components/Header";
 import { Info } from "./components/Info";
 import { Banner } from "./components/Banner";
 
-import { Banner } from './components/Banner';
-import { Header } from "./Components/Header/Header";
-import { Info } from "./components/Info";
 export function App() {
 	return (
 		<div className="layout">
